d17f66c2867f8b2f0c4b54f186ca23c1a2fadf9c
Alexander Dmitriev <alex.golostos@gmail.com>
2024-02-03 20:51:56 +0300

    feat(project): add getters student

--- core/studenthogwards.py
+++ core/studenthogwards.py
@@ -5,6 +5,13 @@ class StudentHogwards:
         self.__mana = mana
         self.__spells = spells
 
+
+    def get_name(self): return self.__name
+    
+    def get_home_hogwards(self): return self.__home_hogwards
+
+    def get_mana(self): return self.__mana
+
     def learn_spell(self, spell): pass
 
     def cast_spell(self): pass
