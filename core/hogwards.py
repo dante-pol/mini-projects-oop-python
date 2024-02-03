b724e2f328b535276c814fea0905827ea9f052c9
Alexander Dmitriev <alex.golostos@gmail.com>
2024-02-03 20:00:10 +0300

    feat(project): add hogwards

---
+++ core/hogwards.py
@@ -0,0 +1,10 @@
+class Hogwards:
+    def __init__(self, students=[], spells=[]):
+        self.__students = students
+        self.__spells = spells
+
+    def enroll_student(self, student): pass
+
+    def teach_spell(self, spell): pass
+
+    def simulate_duel(self, meat1, meat2): pass
