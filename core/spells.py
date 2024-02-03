62750b4e4c9348b29d2cd9626556ae5f66747272
Alexander Dmitriev <alex.golostos@gmail.com>
2024-02-03 19:50:53 +0300

    feat(project): add spells

---
+++ core/spells.py
@@ -0,0 +1,16 @@
+class Spell:
+    def __init__(self,):
+        self.__name = name
+        self.__description = description
+        self.__size_mana = size_mana
+
+    def get_name(self): 
+        return self.__name
+
+    def get_description(self): 
+        return self.__description
+
+    def get_size_mana(self): 
+        return self.__size_mana
+    
+    def cast(self, target): pass
