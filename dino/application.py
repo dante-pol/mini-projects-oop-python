af9b02f52dacb3e506f15443af3bb39157396dc0
Программной Инженерии Школа <dmitry.rak@seschool.ru>
2024-02-07 21:09:55 +0300

    fix(project): fix Application

--- dino/application.py
+++ dino/application.py
@@ -1,8 +1,13 @@
 class Application:
-    def run(): pass
+
+    @staticmethod
+    def run():
+        import core.esosystem
+        ecosystem.create_entityes()
+        simulate()
 
     def stop(): pass
 
     
-    if __name__ == '__main__': pass
-        
+if __name__ == '__main__': 
+    Application.run()
